b52a4079b33ac57c97e6d7cb4ccb71087af7463e
Trey White <trey@2codemonkeys.com>
2015-07-05 11:17:24 -0400

    Initial Scaffolding and getting 2blue.co to match the current 2blue.co homepage

---
+++ config-sample.js
@@ -0,0 +1,9 @@
+var config = {};
+
+config.schemes = ['http'];
+config.host = "localhost:3000";
+
+config.mongo = "mongodb://localhost:27017/";
+config.db = "MONGO_DB_NAME_HERE";
+
+module.exports = config;
